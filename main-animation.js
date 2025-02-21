848e18305893bf2b5bc1643f850761f69fd91b3d
Solaxion <greenkitty67@gmail.com>
2025-02-21 12:19:49 -0600

    main-animation
    updated animation

--- main-animation.js
+++ main-animation.js
@@ -1,4 +1,10 @@
 $(".resume-part h2").click(function(){
-    
+    if($(this).hasClass("active")){
+        $(this.parent().children("resume-item").hide("slow"));
+    }
+    else{
+        $(this).parent().children(".resume-item").show("slow");
+        $(this).parent().children(".resume-item").addClass("active");
+    }
 
 });
