0dd2dc2560ce74e67505db00a8188802e3be52b5
Solaxion <greenkitty67@gmail.com>
2025-02-21 12:35:37 -0600

    animation fix
    fixed parenthesis and added pointer style to cursor in hover mode

--- main-animation.js
+++ main-animation.js
@@ -1,6 +1,6 @@
 $(".resume-part h2").click(function(){
     if($(this).hasClass("active")){
-        $(this.parent().children("resume-item").hide("slow"));
+        $(this).parent().children("resume-item").hide("slow");
     }
     else{
         $(this).parent().children(".resume-item").show("slow");
